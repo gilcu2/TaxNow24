bc92ffb283c617f1c867006be00748c0811a7a07
Reynaldo Gil <gilcu2mail.com>
2025-01-19 17:34:34 +0100

    all checks ok

--- jobscheduler/jobscheduler/main.py
+++ jobscheduler/jobscheduler/main.py
@@ -6,8 +6,9 @@ import click
 @click.command()
 def main(args=None) -> int:
     """Console script for jobscheduler."""
-    click.echo("Replace this message by putting your code into "
-               "jobscheduler.cli.main")
+    click.echo(
+        "Replace this message by putting your code into jobscheduler.cli.main"
+    )
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
